676c4f829c6ca72cb1bca2e683ddaa8ba261f946
Josh Winton <joshcwinton@gmail.com>
2022-01-21 15:47:14 -0500

    initial frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React  from 'react';
+import WordForm from './components/WordForm'
+
+function App() {
+  return (
+    <div className="App">
+      hello
+      <WordForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/WordForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class WordForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: ""};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    fetch(`http://127.0.0.1:5000/check_word?word=${this.state.value}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if(res == true){
+            this.setState({displayText: `${this.state.value} is a valid Jamaican word.`})
+        } else {
+            this.setState({displayText: `${this.state.value} is not a valid Jamaican word.`})
+        }
+      });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Word:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {this.state.displayText}
+      </div>
+    );
+  }
+}
+
+export default WordForm;
