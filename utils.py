48b9080018551b453b95e5bd6c7e51f5cd71115e
Josh Winton <joshcwinton@gmail.com>
2022-02-07 09:57:50 -0500

    change API structure

---
+++ utils.py
@@ -0,0 +1,11 @@
+"""Util functions for JCRDict"""
+import csv
+
+
+def load_dictionary(dictionary_path):
+    """Read dictionary from CSV."""
+    result = {}
+    with open(dictionary_path, encoding='utf-8') as file:
+        for line in csv.DictReader(file, fieldnames=('jamaican', 'english')):
+            result[line['jamaican']] = line['english']
+    return result
